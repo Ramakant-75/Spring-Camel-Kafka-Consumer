3f88a78766c407dd1aa376dc7dc450f881eb8524
Ramakant Gachi <ramakant@gachi.com>
2024-03-11 10:51:11 +0530

    initial commit

---
+++ src/main/java/com/example/consumer/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.example.consumer.constant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constant {
+}
---
+++ src/main/java/com/example/consumer/service/KafkaConsumerService.java
@@ -0,0 +1,25 @@
+package com.example.consumer.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@Slf4j
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "topic-1",groupId = "myGroup")
+    public void consumer(String message, ConsumerRecord<String,String> record){
+        log.info("{} messaged you : {},{}",record.topic(),message,new Date());
+    }
+
+    @KafkaListener(topics = "nikhil",groupId = "myGroup")
+    public void consumeChat(String message, ConsumerRecord<String,String> record){
+        log.info("{} messaged you : {}",record.topic(),message);
+
+    }
+}
